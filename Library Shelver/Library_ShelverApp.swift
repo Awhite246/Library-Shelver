3da1b1a283ece89955fc2a453315208e54f40f55
Alistiar White <awhite@bsd220.org>
2023-02-02 09:44:26 -0600

    Initial Commit

---
+++ Library Shelver/Library_ShelverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Library_ShelverApp.swift
+//  Library Shelver
+//
+//  Created by Alistair White on 2/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Library_ShelverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
