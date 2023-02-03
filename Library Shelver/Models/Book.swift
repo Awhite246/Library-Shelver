97e866824139b994c9c5141098d934d2875ac5d3
Alistiar White <awhite@bsd220.org>
2023-02-03 09:33:45 -0600

    Created folders to organize code, and created a book struct to store book information

---
+++ Library Shelver/Models/Book.swift
@@ -0,0 +1,16 @@
+//
+//  Book.swift
+//  Library Shelver
+//
+//  Created by Alistair White on 2/3/23.
+//
+
+import Foundation
+
+struct Book {
+    var id = UUID()
+    var title : String
+    var dewey : Float
+    var author : String
+    
+}
