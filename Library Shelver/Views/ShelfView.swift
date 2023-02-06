fcc8d1782b7064e38a9fb3ae2a222b71c3f8e669
Alistiar White <awhite@bsd220.org>
2023-02-06 09:58:50 -0600

    Created ShelfView to display books

---
+++ Library Shelver/Views/ShelfView.swift
@@ -0,0 +1,21 @@
+//
+//  ShelfView.swift
+//  Library Shelver
+//
+//  Created by Alistair White on 2/6/23.
+//
+
+import SwiftUI
+
+//displays books and allows dragging around for reordering
+struct ShelfView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ShelfView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShelfView()
+    }
+}
