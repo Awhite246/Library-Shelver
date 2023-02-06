8746f58a4979a1ed51d8476bcd28b70ae8dea785
Alistiar White <awhite@bsd220.org>
2023-02-06 09:51:39 -0600

    Book view moved into book file, book struct made hashable so can be put in a loop

--- Library Shelver/Models/Book.swift
+++ Library Shelver/Models/Book.swift
@@ -7,10 +7,31 @@
 
 import Foundation
 
-struct Book {
+struct Book : Hashable {
     var id = UUID()
     var title : String
     var dewey : Float
     var author : String
-    
 }
+
+import SwiftUI
+
+//Creates a view which will be used for each book dislpayed
+struct BookView: View {
+    let book : Book
+    var body: some View {
+        VStack {
+            //tempory place holder in place of a book image
+            Text("\(book.title)\nBy: \(book.author)\n\(book.dewey)")
+                .multilineTextAlignment(.center)
+                .border(Color.black, width: 1)
+        }
+    }
+}
+
+struct BookView_Previews: PreviewProvider {
+    static var previews: some View {
+        BookView(book: Book(title: "Book", dewey: 031.1, author: "Joe"))
+    }
+}
+
