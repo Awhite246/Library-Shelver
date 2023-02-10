0c80507c40c298530110ef8bf71f94fefcbba95b
Alistiar White <awhite@bsd220.org>
2023-02-10 09:35:15 -0600

    Books can be dragged around

--- Library Shelver/Views/ShelfView.swift
+++ Library Shelver/Views/ShelfView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 //displays books and allows dragging around for reordering
 struct ShelfView: View {
     //placeholder for actual randomized book list
-    @State var bookList = (0...5).map { num in Book(title: "Title", dewey: 123.45, author: "Author", offset: 0) }
+    @State var bookList = (0...5).map { num in Book(title: "Title", dewey: 123.45, author: "Author", offset: (200 - CGFloat(num * 100))) }
+    @State var position = 0.0
     var body: some View {
         /* Sudo Code:
          * Display a bunch of books (ZStack?)
@@ -21,24 +22,30 @@ struct ShelfView: View {
          * Use .onEnded to finalize book positions
          * Update book positions (array?) that checker will use
          */
-        
-        ZStack {
-            HStack {
-                ForEach(0..<6) { i in
-                    BookView(book: bookList[i])
-                        .offset(x: bookList[i].offset)
-                        .gesture(
-                            DragGesture()
-                                .onChanged { gesture in
-                                    bookList[i].offset = gesture.translation.width
-                                    //Dynamicly change positions
-                                    
-                                }
-                                .onEnded { _ in
-                                    
-                                }
-                        )
+        VStack {
+            Text("\(position)")
+            ZStack {
+                Group {
+                    ForEach(0..<6) { i in
+                        BookView(book: bookList[i])
+                            .offset(x: bookList[i].offset)
+                            .gesture(
+                                DragGesture()
+                                    .onChanged { gesture in
+                                        
+                                        bookList[i].offset = gesture.location.x
+                                        //Dynamicly change positions
+                                        //each time the book passes halfway through another book it switches positions
+                                        position = bookList[i].offset
+                                    }
+                                    //update 'start' position of books on release
+                                    .onEnded { _ in
+                                        
+                                    }
+                            )
+                    }
                 }
+                .frame(width: UIScreen.main.bounds.width)
             }
         }
     }
