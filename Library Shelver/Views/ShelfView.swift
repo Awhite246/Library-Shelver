7112031120e21574c2bac34ec6136b8fd6ed203a
Alistiar White <awhite@bsd220.org>
2023-02-14 10:05:59 -0600

    Moved books is bigger, books 'part the water' in one direction, but breaks when switching direction

--- Library Shelver/Views/ShelfView.swift
+++ Library Shelver/Views/ShelfView.swift
@@ -13,6 +13,8 @@ struct ShelfView: View {
     @State var bookList = (0...5).map { num in Book(title: "Book", dewey: 1, author: "\(num)", xPosition: (50 + CGFloat(num * 55))) }
     @State var position = 0.0
     @State var currentBook = -1 //keeps track of which book is being dragged, so more than one book can't be dragged at once
+    @State var frontBook = -1 //keeps track of book infront of current book
+    @State var backBook = -1 //keeps track of book behind of current book
     
     let arraySize = 6 //used for testing, and so changing array size is easier
     
@@ -28,7 +30,7 @@ struct ShelfView: View {
          */
         VStack {
             Text("\(position)")
-            Text("\(currentBook)")
+            Text("Current: \(currentBook)\tBack:\(backBook)\tFront:\(frontBook)")
             ZStack {
                 Group {
                     ForEach(0..<arraySize) { i in
@@ -41,19 +43,27 @@ struct ShelfView: View {
                                         //checks if there is a current book or not
                                         if currentBook < 0 {
                                             currentBook = i
+                                            frontBook = i + 1
+                                            backBook = i - 1
+                                            bookList[i].height += 10
+                                            bookList[i].width += 5
                                         }
-                                        if i == currentBook {
+                                        if i == currentBook { //makes sure only the current book can be dragged
                                             //moves the book your dragging
                                             bookList[i].xPosition = gesture.location.x
                                         }
                                         //Dynamicly change other book positions, parting the waters
                                         
-                                        if i > 0 && bookList[i].xPosition < bookList[i - 1].xPosition { //checking moving left, when position is less than the book behind
-                                            bookList[i - 1].xPosition = CGFloat(50 + (i * 55))
+                                        if backBook >= 0 && bookList[i].xPosition < (bookList[backBook].xPosition + (bookList[backBook].width / 2)) { //checking moving left, when position is less than the book behind
+                                            bookList[backBook].xPosition = CGFloat(50 + ((backBook + 1) * 55))
+                                            frontBook = backBook
+                                            backBook -= 1
                                         }
-                                        if i < arraySize - 1 && bookList[i].xPosition > bookList[i + 1].xPosition {
+                                        if frontBook < arraySize && bookList[i].xPosition > (bookList[frontBook].xPosition - (bookList[frontBook].width / 2)) {
                                             //checking moving right, when position is greater than book ahead
-                                            bookList[i + 1].xPosition = CGFloat(50 + (i * 55))
+                                            bookList[frontBook].xPosition = CGFloat(50 + ((frontBook - 1) * 55))
+                                            backBook = frontBook
+                                            frontBook += 1
                                         }
                                         
                                         position = bookList[i].xPosition
@@ -61,11 +71,15 @@ struct ShelfView: View {
                                     //update 'start' position of books on release
                                     .onEnded { _ in
                                         //resets current book so a new book can be dragged
+                                        bookList[i].height -= 10
+                                        bookList[i].width -= 5
                                         currentBook = -1
+                                        frontBook = -1
+                                        backBook = -1
                                         //sorts the books by position, then spreads them out
                                         bookList.sort { $0.xPosition < $1.xPosition}
-                                        for i in 0..<6 {
-                                            bookList[i].xPosition = CGFloat(50 + (i * 55))
+                                        for j in 0..<6 {
+                                            bookList[j].xPosition = CGFloat(50 + (j * 55))
                                         }
                                     }
                             )
