94fff51f7869cfbde02130a60df30f7fab96cc97
VictorGun <Thunderboomboom7@gmail.com>
2023-02-15 09:21:46 -0600

    added a dewey and fiction view

---
+++ Library Shelver/Views/DeweyView.swift
@@ -0,0 +1,20 @@
+//
+//  DeweyView.swift
+//  Library Shelver
+//
+//  Created by Victor Gunderson on 2/15/23.
+//
+
+import SwiftUI
+
+struct DeweyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DeweyView_Previews: PreviewProvider {
+    static var previews: some View {
+        DeweyView()
+    }
+}
---
+++ Library Shelver/Views/FictionView.swift
@@ -0,0 +1,20 @@
+//
+//  FictionView.swift
+//  Library Shelver
+//
+//  Created by Victor Gunderson on 2/15/23.
+//
+
+import SwiftUI
+
+struct FictionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FictionView_Previews: PreviewProvider {
+    static var previews: some View {
+        FictionView()
+    }
+}
