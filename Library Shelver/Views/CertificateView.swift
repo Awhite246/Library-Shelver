0e140fcb77c91d5457e0200c370aa61fd2f4d780
VictorGun <Thunderboomboom7@gmail.com>
2023-02-27 09:32:21 -0600

    Added the certificate view

---
+++ Library Shelver/Views/CertificateView.swift
@@ -0,0 +1,20 @@
+//
+//  CertificateView.swift
+//  Library Shelver
+//
+//  Created by Victor Gunderson on 2/27/23.
+//
+
+import SwiftUI
+
+struct CertificateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CertificateView_Previews: PreviewProvider {
+    static var previews: some View {
+        CertificateView()
+    }
+}
