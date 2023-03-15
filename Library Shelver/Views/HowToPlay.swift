90b1f48ce37576bffed0d3de200bf17017904106
VictorGun <Thunderboomboom7@gmail.com>
2023-03-15 09:43:27 -0500

    finished the how to play view

--- Library Shelver/Views/HowToPlay.swift
+++ Library Shelver/Views/HowToPlay.swift
@@ -20,15 +20,30 @@ struct HowToPlay: View {
                     .foregroundColor(Color("Bistre"))
                     .padding(30)
                 Text(" - There are two gamemodes")
-            }
-        } .background(
-            ZStack {
-                Image("peach")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 750, height: 200)
-            }
-        )
+                    
+                Text("(Dewey and Fiction)")
+                    .fontWeight(.bold)
+                Text("Click to drag the books in the correct order")
+                Text("Click the CHECK button to see if you positioned the books right.")
+                    .fontWeight(.bold)
+                Text("Repeat the same process in the other gamemode you havent done")
+                Text("If you completed both games without any fails, you will recieve a certificate")
+                    .fontWeight(.bold)
+                Text("Show the certificate to your librarian to get accepted for your position")
+                Text("CONGRADULATIONS!")
+                    .fontWeight(.bold)
+                    .font(.system(size: 50))
+                    .foregroundColor(Color("Bistre"))
+                    .padding(30)
+            } .background(
+                ZStack {
+                    Image("peach")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 750, height: 200)
+                }
+            )
+        }
     }
 }
 
