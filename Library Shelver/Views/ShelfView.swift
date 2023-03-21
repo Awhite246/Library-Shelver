00627fbe67ba8ad22f2fde3d0bdbd839de20075b
VictorGun <Thunderboomboom7@gmail.com>
2023-03-21 09:51:00 -0500

    no errors

--- Library Shelver/Views/ShelfView.swift
+++ Library Shelver/Views/ShelfView.swift
@@ -10,7 +10,7 @@ import AVFoundation
 
 //ShelfView displays books on a shelf that can be dragged around.
 struct ShelfView: View {
-    let allBookInfo = Bundle.main.decocd([BookInfo].self, from: "bookList.json")
+    //let allBookInfo = Bundle.main.decode([BookInfo].self, from: "bookList.json")
     let arraySize = 7 //Used for testing, and so changing array size is easier
     let offSet : CGFloat = 7 //How far apart displayed books are
     
