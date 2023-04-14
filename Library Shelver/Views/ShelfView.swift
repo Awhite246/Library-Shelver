0bbb6fbc7d36532625fd5e5a448cc46c1f11ac69
Alistiar White <awhite@bsd220.org>
2023-04-14 09:42:06 -0500

    fix

--- Library Shelver/Views/ShelfView.swift
+++ Library Shelver/Views/ShelfView.swift
@@ -126,7 +126,7 @@ struct ShelfView: View {
                     totalWidth += offSet
                 }
                 totalWidth -= offSet
-                startingPos = ((screenWidth - totalWidth) / 2) - 100
+                startingPos = ((screenWidth - totalWidth) / 2) - 10
                 
                 bookList.shuffle()
                 sortByPosition()
