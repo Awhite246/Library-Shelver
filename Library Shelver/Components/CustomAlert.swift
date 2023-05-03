49859b1ef991ea05fb29ef3c808c01c289535c01
Alistiar White <awhite@bsd220.org>
2023-05-03 10:04:16 -0500

    Basic layout of custom alert created

--- Library Shelver/Components/CustomAlert.swift
+++ Library Shelver/Components/CustomAlert.swift
@@ -9,13 +9,42 @@ import SwiftUI
 
 struct CustomAlert: View {
     @Binding var showAlert : Bool
+    @State var title : String
+    @State var message : String
+    @State var backgroundColor : Color = Color.white
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text(title)
+                .font(.title2).bold()
+                .foregroundColor(Color.black)
+            Text(message)
+                .foregroundColor(Color.black)
+                .padding(.bottom)
+            HStack {
+                //Left Button
+                Button {
+                    message += "Hi"
+                } label: {
+                    Text("Left Button")
+                }
+                .padding(.horizontal)
+                //Right Button
+                Button {
+                    message.removeLast(2)
+                } label: {
+                    Text("Right Button")
+                }
+                .padding(.horizontal)
+            }
+        }
+        .padding(.all)
+        .background(RoundedRectangle(cornerRadius: 15).foregroundColor(backgroundColor))
+        .preferredColorScheme(.dark)
     }
 }
 
 struct CustomAlert_Previews: PreviewProvider {
     static var previews: some View {
-        CustomAlert(showAlert: .constant(true))
+        CustomAlert(showAlert: .constant(true), title: "Title", message: "Hi")
     }
 }
