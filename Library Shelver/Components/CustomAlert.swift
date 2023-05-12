9f9e5f91c72b4a6a18d568cfec413efafad56a6b
Alistiar White <awhite@bsd220.org>
2023-05-12 09:26:42 -0500

    Added custom background and executable code to custom alert

--- Library Shelver/Components/CustomAlert.swift
+++ Library Shelver/Components/CustomAlert.swift
@@ -11,46 +11,48 @@ struct CustomAlert: View {
     @Binding var showAlert : Bool
     @State var title : String
     @State var message : String
-    @State var backgroundColor : Color = Color.white
+    @State var backgroundColor : Color = Color("Peach")
+    @State var primaryButton : String
+    @State var secondaryButton : String
+    @State var action : () -> Void
     var body: some View {
-        ZStack {
-            //Used to check background visibility / opactiy testing
-            Color.red
-                .frame(width:50,height:50)
-            VStack {
-                Text(title)
-                    .font(.title2).bold()
-                    .foregroundColor(Color.black)
-                Text(message)
-                    .foregroundColor(Color.black)
-                    .padding(.bottom)
-                HStack {
-                    //Left Button
-                    Button {
-                        message += "Hi"
-                    } label: {
-                        Text("Left Button")
-                    }
-                    .padding(.horizontal)
-                    //Right Button
-                    Button {
-                        message.removeLast(2)
-                    } label: {
-                        Text("Right Button")
-                    }
-                    .padding(.horizontal)
+        VStack {
+            Text(title)
+                .font(.title2).bold()
+                .foregroundColor(Color.black)
+            Text(message)
+                .foregroundColor(Color.black)
+                .padding(.bottom)
+            HStack {
+                //Primary Button
+                Button {
+                    action()
+                } label: {
+                    Text("Left Button")
                 }
+                .padding(.horizontal)
+                //Dismiss Button
+                Button {
+                    showAlert = false
+                } label: {
+                    Text("Right Button")
+                }
+                .padding(.horizontal)
             }
-            .padding(.all)
-            .background(RoundedRectangle(cornerRadius: 15).foregroundColor(backgroundColor))
-            .opacity(0.9)
+            
         }
+        .frame(maxWidth: 300)
+        .padding(.all)
+        .background(RoundedRectangle(cornerRadius: 15).foregroundColor(backgroundColor))
+        .opacity(0.95)
         .preferredColorScheme(.dark)
     }
 }
 
 struct CustomAlert_Previews: PreviewProvider {
     static var previews: some View {
-        CustomAlert(showAlert: .constant(true), title: "Title", message: "Hi")
+        CustomAlert(showAlert: .constant(true), title: "Title", message: "Hi", primaryButton: "Button 1", secondaryButton: "Button 2") {
+            //nothing
+        }
     }
 }
